d419ec821dfcd9bfd7866553135a9f95f62ea7a0
MrBiTs <mrbits@mrbits.com.br>
2022-11-09 22:57:51 -0300

    Fix CKV2_AWS_34: "AWS SSM Parameter should be Encrypted"

--- remote_state/ssm.tf
+++ remote_state/ssm.tf
@@ -1,6 +1,13 @@
+resource "aws_kms_key" "aws_ssm_key" {
+  description             = "AWS SSM KMS key"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+}
+
 resource "aws_ssm_parameter" "locks_table_arn" {
   name      = format("%s/tf-locks-table-arn", local.ssm_prefix)
-  type      = "String"
+  type      = "SecureString"
+  key       = aws_kms_key.aws_ssm_key.arn
   value     = aws_dynamodb_table.lock_table.arn
   overwrite = false
 
@@ -16,7 +23,8 @@ resource "aws_ssm_parameter" "locks_table_arn" {
 
 resource "aws_ssm_parameter" "remote_state_bucket" {
   name      = format("%s/tf-remote-state-bucket", local.ssm_prefix)
-  type      = "String"
+  type      = "SecureString"
+  key       = aws_kms_key.aws_ssm_key.arn
   value     = aws_s3_bucket.remote_state.id
   overwrite = false
 
