3584dac1a64bed405bf732a59ae152e40d6841eb
MrBiTs <mrbits@mrbits.com.br>
2022-11-09 22:49:25 -0300

    Fix CKV_AWS_119: "Ensure DynamoDB Tables are encrypted using a KMS Customer Managed CMK"

--- remote_state/dynamodb.tf
+++ remote_state/dynamodb.tf
@@ -1,3 +1,9 @@
+resource "aws_kms_key" "aws_dynamodb_key" {
+  description             = "AWS DynamoDB KMS key"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+}
+
 resource "aws_dynamodb_table" "lock_table" {
   name           = format("%s-dynamodb", local.prefix)
   billing_mode   = "PROVISIONED"
@@ -13,5 +19,10 @@ resource "aws_dynamodb_table" "lock_table" {
 
   point_in_time_recovery {
     enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.aws_dynamodb_key.arn
   }
 }
